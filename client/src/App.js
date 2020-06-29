2ed47b384b14c15b3d49efd8e37339a9b99703f1
Marisa Nest <marisa.nest@hhv.de>
2020-06-29 18:49:50 +0200

    Migrate react-n-body

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import DataGenerator from './components/DataGenerator'
+import Heading from './components/Heading'
+import Plot from './components/Plot'
+
+const dataGenerator = DataGenerator()
+
+const App = () => {
+    const [index, setIndex] = useState(0)
+
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            dataGenerator.next()
+            setIndex(index + 1)
+        }, 2000)
+        return () => clearTimeout(timer)
+    }, [index, setIndex])
+
+    const data = dataGenerator.getData()
+
+    return (
+        <div className="App">
+            <Heading iteration={data.iteration + 1}/>
+            <Plot data={data.data} />
+        </div>
+    );
 }
 
 export default App;
